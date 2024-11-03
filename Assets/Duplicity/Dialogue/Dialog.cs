2d2adae36753242175aae77d914cccb69e31f09f
kimbodle <103980156+kimbodle@users.noreply.github.com>
2024-11-03 22:15:54 +0900

    Edit Dialogue System
    - 다이얼로그에 Player 추가
    - 피난묘 vs 플레이어 대화 기능 추가

--- Assets/Duplicity/Dialogue/Dialog.cs
+++ Assets/Duplicity/Dialogue/Dialog.cs
@@ -4,4 +4,5 @@ using UnityEngine;
 public class Dialog : ScriptableObject // ScriptableObject로 변경
 {
     public string[] sentences; // 다이얼로그 문장 배열
+    public bool[] isPlayerSpeaking; // 각 문장의 화자가 플레이어인지 여부
 }
